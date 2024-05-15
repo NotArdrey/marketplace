22e97406734be49cbedaebc8f8c092f781bbabf5
Lanz <manlapazlanz04@gmail.com>
2024-05-15 15:02:08 +0800

    first commit

---
+++ components/navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbar-left">
+            <a href=""><div class="nav-div">Home</div></a>
+            <a href=""><div class="nav-div">Categories</div></a>
+            <a href=""><div class="nav-div">About Us</div></a>
+            <a href=""><div class="nav-div">Contact Us</div></a>
+            <a href=""><div class="nav-div">Settings</div></a>
+        </div>
+        <div class="navbar-right">
+            <p>P57.00</p>
+            <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ pages/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../styles/index.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
+</head>
+<body>
+    <div class="login-left">
+        <div class="upper-login"><h2>NUB Marketplace</h2></div>
+        <div class="lower-login">
+            <h1 class="login-h1">Login To Your Account</h1>
+            <form action="" method="" id="login-form">
+                <input type="text" placeholder="NU Email" class="input-box">
+                <input type="password" placeholder="Password" class="input-box">
+                <input type="submit" value="Sign In" class="login-button">
+            </form>
+        </div>
+    </div>
+    <div class="login-right">
+        <div class="triangle"></div>
+        <div class="small-triangle"></div>
+        <div class="login-right-container">
+            <h1>New Here?</h1>
+            <p>Sign up and discover a great amount of new items!</p>
+            <button><a href="../pages/registration.php">Sign Up</a></button>
+        </div>
+    </div>
+</body>
+</html>
