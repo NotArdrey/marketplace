c966fbd60ff49c33f95d2407500464bac9db58ec
Lanz <manlapazlanz04@gmail.com>
2024-05-17 11:11:10 +0800

    add product

--- pages/add_product.php
+++ pages/add_product.php
@@ -8,7 +8,21 @@
 <body>
     <div class="settings-container">
         <?php  include_once '../components/sidebar.php'; ?>
-        
+        <div class="add-product-container">
+            <form action="">
+                <label for="">Product Name</label>
+                <input type="text"><br>
+                <label for="">Product Description</label>
+                <input type="text"><br>
+                <label for="">Product Price</label>
+                <input type="number"><br>
+                <label for="">Product Stock</label>
+                <input type="number"><br>
+                <label for="">Upload Picture</label>
+                <input type="file" name="productImg" id="productImg" accept=".jpg, .jpeg, .png, .avif"/><br>
+                <input type="submit" value="Add Product" name="createProduct">
+            </form>
+        </div>
     </div>
 </body>
 </html>
