6107ba2669d856baef842a5f829411a1af538c7b
Aiyuuuuuu <neilardrey14@gmail.com>
2024-05-18 22:26:51 +0800

    new file:   pages/security.php modified:   styles/index.css

---
+++ pages/security.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Products</title>
+    <link rel="stylesheet" href="../styles/index.css">
+</head>
+<body>
+    <div class="settings-container">
+        <?php  include_once '../components/sidebar.php'; ?>
+
+        <div class="Password-container">
+            <div class="Header">
+                <h1>Change Password</h1>
+            </div>
+        </div>
+
+        <div class="text-container">
+            <div class="information-text">
+                <p>Your password must be at least 6 characters and should include a combination of numbers, letters and special characters (!$@%).</p>
+            </div>
+
+            <form action="" method="" id="registration-form"></form>
+            <div class="row-container">
+
+                <div class="rows">
+                    Current Password
+                    <div>
+                        <input type="text" placeholder="Enter Current Password" class="Security-input-box">
+                    </div>
+                </div>
+                <div class="rows">
+                    New Password
+                    <div>
+                        <input type="text" placeholder="Enter New Password" class="Security-input-box">
+                    </div>
+                </div>
+                <div class="rows">
+                    Re-type New Password
+                    <div>
+                        <input type="text" placeholder="Re-type New Password" class="Security-input-box">
+                        <a href="">Forgotten your password?</a>
+                    </div>    
+                </div>
+
+
+                <div class="Security-submit">
+                    <button class = Security-cancel><a href="">Cancel</a></button>
+                    <button class = Security-save><a href="">Save</a></button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
