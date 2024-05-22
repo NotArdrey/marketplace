bdd741da1f175b859d903127dcd5fbe45b728605
Aiyuuuuuu <neilardrey14@gmail.com>
2024-05-22 15:10:55 +0800

    new file:   pages/change_password.php new file:   pages/otp_verification.php modified:   pages/security.php modified:   styles/index.css

--- pages/security.php
+++ pages/security.php
@@ -21,33 +21,33 @@
                 <p>Your password must be at least 6 characters and should include a combination of numbers, letters and special characters (!$@%).</p>
             </div>
 
-            <form action="" method="" id="registration-form"></form>
+            <form action="" method="" id="registration-form">
             <div class="row-container">
 
                 <div class="rows">
                     Current Password
                     <div>
-                        <input type="text" placeholder="Enter Current Password" class="Security-input-box">
+                        <input type="text" placeholder="Enter Current Password" class="security-input-box">
                     </div>
                 </div>
                 <div class="rows">
                     New Password
                     <div>
-                        <input type="text" placeholder="Enter New Password" class="Security-input-box">
+                        <input type="text" placeholder="Enter New Password" class="security-input-box">
                     </div>
                 </div>
                 <div class="rows">
                     Re-type New Password
                     <div>
-                        <input type="text" placeholder="Re-type New Password" class="Security-input-box">
+                        <input type="text" placeholder="Re-type New Password" class="security-input-box">
                         <a href="">Forgotten your password?</a>
                     </div>    
                 </div>
+                </form>
 
-
-                <div class="Security-submit">
-                    <button class = "Security-cancel"><a href="">Cancel</a></button>
-                    <button class = "Security-save"><a href="">Save</a></button>
+                <div class="security-submit">
+                    <button class = security-cancel><a href="">Cancel</a></button>
+                    <button class = security-save><a href="">Save</a></button>
                 </div>
                 
             </div>
