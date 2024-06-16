da4135067aa46fdb33a738d20e12e4df79af50df
Lanz <manlapazlanz04@gmail.com>
2024-06-17 00:50:09 +0800

    bug fix

--- crud/send_code_password.php
+++ crud/send_code_password.php
@@ -12,8 +12,8 @@ function sendemail_code($first_name, $email, $token_pass) {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; 
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'sample.gmail';
-        $mail->Password   = '******'; 
+        $mail->Username   = 'neilardrey14@gmail.com';
+        $mail->Password   = 'fvvg zyoq qodj qhwc'; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
         $mail->Port       = 587; 
 
